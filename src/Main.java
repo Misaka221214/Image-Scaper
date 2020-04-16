6c9cf210bbf4e9f9e7b3e114a467f753f8166849
Muhan Li <mlfromchina@gmail.com>
2020-04-16 02:09:57 -0700

    added user input

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String path = "C:\\Users\\Administrator.SC-201912271441\\Desktop\\test\\";
-        String sourceLink = "https://warthunder.com/en";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter output path:");
+        String path = in.nextLine() + "\\";
+        System.out.println("Please enter url:");
+        String sourceLink = in.nextLine();
         int count = 0;
         try {
             final Document document = Jsoup.connect(sourceLink).timeout(3000).get();
             for (Element s : document.select("img")) {
-//                System.out.println(s);
-                System.out.println(s.attr("abs:src"));
+//                System.out.println(s.attr("abs:src"));
                 File out = new File(path + count + ".png");
                 new Thread(new Downloader(s.attr("abs:src"), out)).start();
                 count++;
